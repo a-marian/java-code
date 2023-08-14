5c00bf5180104ef39faf1b2809495ec2fcdbd11c
marelas <marela.as@gmail.com>
2023-08-13 23:50:48 -0300

    validator generic

---
+++ src/abcde/validator/Validation.java
@@ -0,0 +1,20 @@
+package abcde.validator;
+
+public interface Validation <K> {
+
+    ValidationResult test(K param);
+
+    default Validation<K> and(Validation<K> other){
+        return (param) -> {
+            ValidationResult firstResult = this.test(param);
+            return  !firstResult.isValid() ? firstResult: other.test(param);
+        };
+    }
+
+    default Validation<K> or(Validation<K> other){
+        return (param) -> {
+            ValidationResult firstResult = this.test(param);
+            return firstResult.isValid() ? firstResult : other.test(param);
+        };
+    }
+}
